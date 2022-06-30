3746867802f6630e5acdece6ef87a42790d5b25e
IvanFebriansyah <87108996+IvanFebriansyah@users.noreply.github.com>
2022-07-01 02:32:45 +0700

    Add files via upload

---
+++ SerialCom/Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnScan = New System.Windows.Forms.Button()
+        Me.CmbPort = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CmbBaud = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.BtnDiscon = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'BtnScan
+        '
+        Me.BtnScan.Location = New System.Drawing.Point(12, 22)
+        Me.BtnScan.Name = "BtnScan"
+        Me.BtnScan.Size = New System.Drawing.Size(93, 26)
+        Me.BtnScan.TabIndex = 0
+        Me.BtnScan.Text = "Scan Port"
+        Me.BtnScan.UseVisualStyleBackColor = True
+        '
+        'CmbPort
+        '
+        Me.CmbPort.FormattingEnabled = True
+        Me.CmbPort.Location = New System.Drawing.Point(120, 25)
+        Me.CmbPort.Name = "CmbPort"
+        Me.CmbPort.Size = New System.Drawing.Size(176, 23)
+        Me.CmbPort.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(66, 15)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Baud Rate :"
+        '
+        'CmbBaud
+        '
+        Me.CmbBaud.AutoCompleteCustomSource.AddRange(New String() {"1200", "2400", "4800", "19200", "38400", "57600", "115200"})
+        Me.CmbBaud.FormattingEnabled = True
+        Me.CmbBaud.Items.AddRange(New Object() {"1200", "2400", "4800", "9600", "19200", "38400", "57600", "115200"})
+        Me.CmbBaud.Location = New System.Drawing.Point(120, 54)
+        Me.CmbBaud.Name = "CmbBaud"
+        Me.CmbBaud.Size = New System.Drawing.Size(176, 23)
+        Me.CmbBaud.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(21, 89)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 53)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Connect"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'BtnDiscon
+        '
+        Me.BtnDiscon.Location = New System.Drawing.Point(21, 89)
+        Me.BtnDiscon.Name = "BtnDiscon"
+        Me.BtnDiscon.Size = New System.Drawing.Size(75, 53)
+        Me.BtnDiscon.TabIndex = 5
+        Me.BtnDiscon.Text = "Disconnect"
+        Me.BtnDiscon.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(323, 184)
+        Me.Controls.Add(Me.BtnDiscon)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CmbBaud)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CmbPort)
+        Me.Controls.Add(Me.BtnScan)
+        Me.Name = "Form1"
+        Me.Text = "SerialCom"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnScan As Button
+    Friend WithEvents CmbPort As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CmbBaud As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents BtnDiscon As Button
+End Class
---
+++ SerialCom/Form1.vb
@@ -0,0 +1,64 @@
+﻿Imports System.IO.Ports
+
+Public Class Form1
+    Dim SerialPort1 As New SerialPort()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.CenterToParent()
+        Button2.Enabled = False
+        BtnDiscon.BringToFront()
+
+        BtnDiscon.Enabled = False
+        BtnDiscon.SendToBack()
+
+        CmbBaud.SelectedItem = "9600"
+    End Sub
+
+    Private Sub BtnScan_Click(sender As Object, e As EventArgs) Handles BtnScan.Click
+        CmbPort.Items.Clear()
+        Dim myPort As Array
+        Dim i As Integer
+        myPort = System.IO.Ports.SerialPort.GetPortNames()
+        CmbPort.Items.AddRange(myPort)
+        i = CmbPort.Items.Count
+        i = i - i
+        Try
+            CmbPort.SelectedIndex = i
+        Catch ex As Exception
+            Dim result As DialogResult
+            result = MessageBox.Show("com port not detected", "Warning", MessageBoxButtons.OK)
+            CmbPort.Text = ""
+            CmbPort.Items.Clear()
+            Call Form1_Load(Me, e)
+        End Try
+        Button2.Enabled = True
+        Button2.BringToFront()
+        CmbPort.DroppedDown = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+
+
+        Button2.Enabled = False
+        Button2.SendToBack()
+
+        SerialPort1.BaudRate = CmbBaud.SelectedItem
+        SerialPort1.PortName = CmbPort.SelectedItem
+
+        SerialPort1.Open()
+
+        BtnDiscon.Enabled = True
+        BtnDiscon.BringToFront()
+    End Sub
+
+    Private Sub BtnDiscon_Click(sender As Object, e As EventArgs) Handles BtnDiscon.Click
+        BtnDiscon.Enabled = False
+        BtnDiscon.SendToBack()
+
+        SerialPort1.Close()
+
+        Button2.Enabled = True
+        Button2.BringToFront()
+
+    End Sub
+End Class
